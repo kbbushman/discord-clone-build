5c78171deb6f8366f28d139e721d391ebcab6661
kbbushman <k.bushman@yahoo.com>
2021-04-04 23:58:35 -0700

    Adds routes for unauthenticated user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-// import React from "react";
-// import { Route, Switch } from "react-router-dom";
-// import Account from "./pages/Account";
-// import AuthRoute from "./pages/AuthRoute";
-// import ForgotPassword from "./pages/ForgotPassword";
-// import Home from "./pages/Home";
-// import Invite from "./pages/Invite";
-// import Landing from "./pages/Landing";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import ResetPassword from "./pages/ResetPassword";
-// import ViewGuild from "./pages/ViewGuild";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import Account from './pages/Account';
+// import AuthRoute from './pages/AuthRoute';
+import ForgotPassword from './pages/ForgotPassword';
+// import Home from './pages/Home';
+// import Invite from './pages/Invite';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ResetPassword from './pages/ResetPassword';
+// import ViewGuild from './pages/ViewGuild';
 
 export default function App() {
   return (
-    <>
-      <div>routes</div>
-    </>
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password/:token" component={ResetPassword} />
+    </Switch>
   );
 }
