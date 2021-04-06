0b66f79aae20420e0aa3678fdb63193e4626f686
kbbushman <k.bushman@yahoo.com>
2021-04-05 19:11:10 -0700

    Adds axios client config

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,3 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const api = axios.create();
+export const api = axios.create({
+  baseURL: 'http://localhost:4000/api',
+  withCredentials: true,
+});
