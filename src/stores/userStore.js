e1109703294b002396a15e2052a50e8fc6f3e702
kbbushman <k.bushman@yahoo.com>
2021-04-06 23:49:53 -0700

    Adds zustand user store with auth methods

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,8 +1,18 @@
-import create from "zustand";
-import { persist } from "zustand/middleware";
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const userStore = create(() => ({
-  current: null,
-}));
+const userStore = create(
+  persist(
+    (set, get) => ({
+      current: null,
+      setUser: (user) => set({ current: user }),
+      logout: () => set({ current: null }),
+      isAuth: () => get().current !== null,
+    }),
+    {
+      name: 'user-storage',
+    }
+  )
+);
 
 export default userStore;
