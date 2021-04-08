8fce9f60901154e7a9d6d9f460172fd60cb2c832
kbbushman <k.bushman@yahoo.com>
2021-04-07 23:32:33 -0700

    Adds formik form state, adds submit handler

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,17 +6,38 @@ import {
   Image,
   Link,
   Text,
-} from "@chakra-ui/react";
-import { login } from "api/handler/auth";
-import { Form, Formik } from "formik";
-import React from "react";
-import { Link as RLink, useHistory } from "react-router-dom";
-import userStore from "stores/userStore";
-import toErrorMap from "utils/toErrorMap";
-import InputField from "components/shared/InputField";
-import { LoginSchema } from "validation/auth.schema";
+} from '@chakra-ui/react';
+import { login } from 'api/handler/auth';
+import { Form, Formik } from 'formik';
+import React from 'react';
+import { Link as RLink, useHistory } from 'react-router-dom';
+import userStore from 'stores/userStore';
+import toErrorMap from 'utils/toErrorMap';
+import InputField from 'components/shared/InputField';
+import { LoginSchema } from 'validation/auth.schema';
 
 export default function Login() {
+  const history = useHistory();
+  const setUser = userStore((state) => state.setUser);
+
+  async function handleSubmit(values, { setErrors }) {
+    try {
+      const { data } = await login(values);
+      if (data) {
+        setUser(data);
+        history.push('/channels/me');
+      }
+    } catch (err) {
+      if (err?.response?.status === 401) {
+        setErrors({ password: 'Invalid credentials' });
+      } else if (err?.response?.status === 404) {
+        setErrors({ email: 'No user found' });
+      } else {
+        setErrors(toErrorMap(err));
+      }
+    }
+  }
+
   return (
     <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
       <Box px={4} width="full" maxWidth="500px" textAlign="center">
@@ -28,50 +49,65 @@ export default function Login() {
             <Heading fontSize="24px">Welcome Back</Heading>
           </Box>
           <Box my={4} textAlign="left">
-            <Form>
-              <InputField
-                label="Email"
-                name="email"
-                autoComplete="email"
-                type="email"
-              />
-              <InputField
-                label="password"
-                name="password"
-                autoComplete="password"
-                type="password"
-              />
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+              }}
+              validationSchema={LoginSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <InputField
+                    label="Email"
+                    name="email"
+                    autoComplete="email"
+                    type="email"
+                  />
+                  <InputField
+                    label="password"
+                    name="password"
+                    autoComplete="password"
+                    type="password"
+                  />
 
-              <Box mt={4}>
-                <Link
-                  as={RLink}
-                  to="/forgot-password"
-                  textColor="highlight.standard"
-                >
-                  Forgot Password?
-                </Link>
-              </Box>
+                  <Box mt={4}>
+                    <Link
+                      as={RLink}
+                      to="/forgot-password"
+                      textColor="highlight.standard"
+                    >
+                      Forgot Password?
+                    </Link>
+                  </Box>
 
-              <Button
-                background="highlight.standard"
-                color="white"
-                width="full"
-                mt={4}
-                type="submit"
-                isLoading={false}
-                _hover={{ bg: "highlight.hover" }}
-                _active={{ bg: "highlight.active" }}
-                _focus={{ boxShadow: "none" }}
-              >
-                Login
-              </Button>
-              <Text mt="4">
-                Don't have an account yet?{" "}
-                <Link as={RLink} to="/register" textColor="highlight.standard">
-                  Sign Up
-                </Link>
-              </Text>
-            </Form>
+                  <Button
+                    background="highlight.standard"
+                    color="white"
+                    width="full"
+                    mt={4}
+                    type="submit"
+                    isLoading={isSubmitting}
+                    _hover={{ bg: 'highlight.hover' }}
+                    _active={{ bg: 'highlight.active' }}
+                    _focus={{ boxShadow: 'none' }}
+                  >
+                    Login
+                  </Button>
+                  <Text mt="4">
+                    Don't have an account yet?{' '}
+                    <Link
+                      as={RLink}
+                      to="/register"
+                      textColor="highlight.standard"
+                    >
+                      Sign Up
+                    </Link>
+                  </Text>
+                </Form>
+              )}
+            </Formik>
           </Box>
         </Box>
       </Box>
