831fcc81d1942ff5366aa355a25f405d39c7a6c5
kbbushman <k.bushman@yahoo.com>
2021-04-08 23:58:31 -0700

    Adds auth route with component or redirect

--- src/pages/AuthRoute.js
+++ src/pages/AuthRoute.js
@@ -1,7 +1,20 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import userStore from "stores/userStore";
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import userStore from 'stores/userStore';
 
-export default function AuthRoute() {
-  return <Route />;
+export default function AuthRoute({ component: Component, ...rest }) {
+  const current = userStore((state) => state.current);
+  const storage = JSON.parse(localStorage.getItem('user-storage'));
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        current || storage?.state?.current ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
 }
