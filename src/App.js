b305c2ccd1dfb2b8662ecead8ebf0fc91b06ef01
kbbushman <k.bushman@yahoo.com>
2021-04-09 21:41:01 -0700

    Adds routes for authenticated user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import Account from './pages/Account';
-// import AuthRoute from './pages/AuthRoute';
+import Account from './pages/Account';
+import AuthRoute from './pages/AuthRoute';
 import ForgotPassword from './pages/ForgotPassword';
-// import Home from './pages/Home';
-// import Invite from './pages/Invite';
+import Home from './pages/Home';
+import Invite from './pages/Invite';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
-// import ViewGuild from './pages/ViewGuild';
+import ViewGuild from './pages/ViewGuild';
 
 export default function App() {
   return (
@@ -19,6 +19,15 @@ export default function App() {
       <Route path="/register" component={Register} />
       <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/reset-password/:token" component={ResetPassword} />
+      <AuthRoute exact path="/channels/me" component={Home} />
+      <AuthRoute exact path="/channels/me/:channelId" component={Home} />
+      <AuthRoute
+        exact
+        path="/channels/:guildId/:channelId"
+        component={ViewGuild}
+      />
+      <AuthRoute exact path="/account" component={Account} />
+      <AuthRoute exact path="/:link" component={Invite} />
     </Switch>
   );
 }
