d4a697a08cec1e9e48fcc53a85c812e64edbc9d4
kbbushman <k.bushman@yahoo.com>
2021-04-12 23:43:55 -0700

    Adds axios response interceptor for 403 errors to clear token and redirect

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,3 +4,21 @@ export const api = axios.create({
   baseURL: 'http://localhost:4000/api',
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { status } = error.response;
+    switch (status) {
+      case 403:
+        localStorage.removeItem('user-storage');
+        window.location.replace('/login');
+        break;
+      default:
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
